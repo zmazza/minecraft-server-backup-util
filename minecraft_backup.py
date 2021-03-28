cee4757782d365183a7f3c425dbf371c3542a672
Zac Mazza <zac.mazza85@gmail.com>
2021-03-27 21:34:23 -0500

    Flake8 and pylint cleanup

--- minecraft_backup.py
+++ minecraft_backup.py
@@ -1,7 +1,8 @@
 """
 Owner: Zac Mazza
 Date: 27 MAR 2021
-Purpose: A utility for backing up a Spigot minecraft server on a Linux machine, zipping the required folders, and loading
+Purpose: A utility for backing up a Spigot minecraft server on a Linux machine,
+zipping the required folders, and loading
 that zip into a destination directory.
 """
 
@@ -17,9 +18,9 @@ def zip_dir(directory, zipname):
     Compresses a directory (ZIP file).
     """
     if os.path.exists(directory):
-        outZipFile = zipfile.ZipFile(zipname, 'w', zipfile.ZIP_DEFLATED)
+        out_zip_file = zipfile.ZipFile(zipname, 'w', zipfile.ZIP_DEFLATED)
 
-        # The root directory within the ZIP file. 
+        # The root directory within the ZIP file.
         rootdir = os.path.basename(directory)
 
         for dirpath, dirnames, filenames in os.walk(directory):
@@ -31,67 +32,71 @@ def zip_dir(directory, zipname):
                 parentpath = os.path.relpath(filepath, directory)
                 arcname = os.path.join(rootdir, parentpath)
 
-                outZipFile.write(filepath, arcname)
-    
-    outZipFile.close()
+                out_zip_file.write(filepath, arcname)
+
+    out_zip_file.close()
 
 
 print("Script started...")
 
 # Directories
-root_src_dir = "/home/pi/minecraft-server"
-root_tmp_dir = "/tmp/tmp_minecraft_backups"
-root_dst_dir = "/home/pi/minecraft-backups"
+ROOT_SRC_DIR = "/home/pi/minecraft-server"
+ROOT_TMP_DIR = "/tmp/tmp_minecraft_backups"
+ROOT_DST_DIR = "/home/pi/minecraft-backups"
 
 path = Path('/tmp/tmp_minecraft_backups')
 if os.path.isdir(path):
     shutil.rmtree(path)
 
-source = os.listdir(root_src_dir)
-files_to_backup = ['banned-ips.json', 'banned-players.json', 'ops.json', 'server.properties']
+source = os.listdir(ROOT_SRC_DIR)
+files_to_backup = [
+    'banned-ips.json',
+    'banned-players.json',
+    'ops.json',
+    'server.properties']
 directories_to_backup = ['plugins', 'world', 'world_nether', 'world_the_end']
 
 for directories in source:
-    if(directories in directories_to_backup):
-        directory_to_move = root_src_dir + "/" + directories
-        new_directory = root_tmp_dir + "/" + directories
+    if directories in directories_to_backup:
+        directory_to_move = ROOT_SRC_DIR + "/" + directories
+        new_directory = ROOT_TMP_DIR + "/" + directories
         print("Copying {} to {}...".format(directory_to_move, new_directory))
-        
+
         shutil.copytree(directory_to_move, new_directory)
 
 for files in source:
-    if(files in files_to_backup):
-        file_to_move = os.path.join(root_src_dir, files)
-        new_file_name = os.path.join(root_tmp_dir, files)
+    if files in files_to_backup:
+        file_to_move = os.path.join(ROOT_SRC_DIR, files)
+        new_file_name = os.path.join(ROOT_TMP_DIR, files)
         print("Copying {} to {}...".format(file_to_move, new_file_name))
-        if(os.path.isfile(file_to_move) != True):
+        if os.path.isfile(file_to_move) is not True:
             print("File does not exist.")
         else:
-            shutil.copy(file_to_move, new_file_name)      
-    
+            shutil.copy(file_to_move, new_file_name)
+
 # Zip the remaining files
 timestr = time.strftime("%Y%m%d-%H%M%S")
 zip_filename = timestr + "_minecraft_backup.zip"
 
 print("Zipping directory...")
-zip_dir(root_tmp_dir, zip_filename)
+zip_dir(ROOT_TMP_DIR, zip_filename)
 
 time.sleep(2)
 
 # Move the zip file to the backup directory
 print("Moving zipped file...")
-shutil.move("" + zip_filename, root_dst_dir)
+shutil.move("" + zip_filename, ROOT_DST_DIR)
 
 # Delete the temporary directory
 print("Removing temp dir...")
-shutil.rmtree(root_tmp_dir)
+shutil.rmtree(ROOT_TMP_DIR)
 time.sleep(2)
 
 # Delete the OLDEST zip over 2 backups
 print("Deleting oldest backup...")
-backups = os.listdir(root_dst_dir)
-while len(os.listdir(root_dst_dir)) > 2:
-    backups = os.listdir(root_dst_dir)
+backups = os.listdir(ROOT_DST_DIR)
+while len(os.listdir(ROOT_DST_DIR)) > 2:
+    backups = os.listdir(ROOT_DST_DIR)
     file_to_remove = backups[0]
-    os.remove(os.path.join(root_dst_dir, file_to_remove))
+    os.remove(os.path.join(ROOT_DST_DIR, file_to_remove))
     time.sleep(2)
